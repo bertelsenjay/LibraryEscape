e3100f9d7e74c06176eed27315171356db92b919
Cooper Carey <CooperCareyGameDev@gmail.com>
2023-04-25 10:25:48 -0700

    fibonacci scripts

---
+++ Library Escape/Assets/Scripts/FibbonacciPuzzle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FibbonacciPuzzle : MonoBehaviour
+{
+    public static bool firstPuzzleAnswered = false;
+    public static bool secondPuzzleAnswered = false;
+    public static bool thirdPuzzleAnswered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
